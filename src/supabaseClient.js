a0ecfae713abf87c9923485cb7bdfabf10da36a1
ego <ely031237@gmail.com>
2025-11-13 12:58:03 +0000

    Ajout de la configuration supabase et fichier test

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// src/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://msstkhwzrlknytgqxvin.supabase.co";      // défini dans .env
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zc3RraHd6cmxrbnl0Z3F4dmluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MzE4NDUsImV4cCI6MjA3ODIwNzg0NX0.oDZJpMiIZ_yY7XzqysxraJqS4guywr_yn5O3MthKddk; // défini dans .env"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
